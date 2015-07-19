efb2b46f59149839995a4960a9d174da4a2860af
shushu.i <shushu.i@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-19 09:45:40 +0000

    Update plugin to avoid loading SDK when not needed.
    git-svn-id: https://plugins.svn.wordpress.org/apester-interactive-content/tags/1.12@1201870 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- qmerce.php
+++ qmerce.php
@@ -7,7 +7,7 @@ Plugin Name: Apester Interactive Content
 Plugin URI: http://apester.co/
 Description: The Apester Interactive Content plugin allows anyone to easily and freely create, embed and share interactive, playful and related content items (polls, trivia, etc.) into posts and articles, in a matter of seconds.
 If you wish for better engagement, virality, circulation, native advertisement campaigns and monetization results, you came to the right place!
-Version: 1.11
+Version: 1.12
 Author: Apester
 Author URI: http://apester.com/
 License: GPLv2 or later
@@ -31,7 +31,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 define( 'QMERCE_MINIMUM_WORDPRESS_VERSION', '2.8' );
-define( 'QMERCE_VERSION', '1.11' );
+define( 'QMERCE_VERSION', '1.12' );
 define( 'QMERCE_SDK_VERSION', 'v1.1' );
 // For dev: define( 'QMERCE_SDK_VERSION', 'dev' );
 define( 'QMERCE_INTERACTION_BASEURL', 'http://interaction.qmerce.com' );
@@ -57,12 +57,10 @@ require_once( QMERCE_PLUGIN_DIR . 'inc/qmerce-automation.php' );
 require_once( QMERCE_PLUGIN_DIR . 'inc/qmerce-tag-composer.php' );
 require_once( QMERCE_PLUGIN_DIR . 'inc/qmerce-shortcodes.php' );
 
-add_action( 'wp_enqueue_scripts', 'qmerce_add_sdk' );
 add_action('widgets_init', 'qmerce_register_widgets');
 add_filter('the_content', array(new QmerceAutomation(), 'renderHtml'));
 
-// Add SDK
-function qmerce_add_sdk() {
+function qmerce_add_sdk_for_shortcode( ) {
     $configuration = array(
         'rendererBaseUrl' => QMERCE_RENDERER_BASEURL,
         'randomBaseUrl' => QMERCE_RANDOM_BASEURL
